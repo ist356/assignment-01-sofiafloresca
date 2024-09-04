4c636db9c912558f4474601f0a942f1e7595f483
Sofia Floresca <sofiafloresca12@gmail.com>
2024-09-04 19:30:54 -0400

    I am pretty sure I did everything right but the tests are failing due to a FileNotFoundError.

--- code/numbers.py
+++ code/numbers.py
@@ -12,3 +12,15 @@ The output should be:
 {'odd': [3, 5], 'even': [2, 4, 6]}
 '''
 
+even = []
+odd = []
+num_dict = {'odd': odd, 'even': even}
+num = int(input("Enter an integer value: "))
+while num != 0:
+    if num % 2 == 0:
+        even.append(num)
+    else:
+        odd.append(num)
+    num = int(input("Enter an integer value: "))
+print(num_dict)
+
